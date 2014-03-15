a8ee2ca3fc522dbff183c7173a9f79fb9739ae3e
Bogdan Vancea <bogdan.aurel.vancea@gmail.com>
2014-03-15 16:20:15 +0100

    #1: Locality Sensitive Hashing Implemented min-hashing for a variable number of hash functions.

--- lsh/code/mapper_template.py
+++ lsh/code/mapper_template.py
@@ -5,28 +5,42 @@ import sys
 
 
 def generate_hash_function():
-    return np.random.random_integers(1, 10000, 3)
+    return np.random.random_integers(1, 100, 3)
 
 
-def hash_f(hash_func, shingle):
+def generate_hash_functions(size):
+    return [generate_hash_function() for _ in range(0, size)]
+
+
+def compute_hash(hash_func, shingle):
     a = hash_func[0]
     b = hash_func[1]
     c = hash_func[2]
     return (a * shingle + b) % c
 
 
+def compute_min_hash(hash_func, shingles):
+    hashed_shingles = [compute_hash(hash_func, x) for x in shingles]
+    return min(hashed_shingles)
+
+
+def compute_hash_for_functions(hash_functions, shingles):
+    sign_matrix_col = [compute_min_hash(hash_func=hf, shingles=shingles) for hf in hash_functions]
+    return sign_matrix_col
+
+
 def partition(video_id, shingles):
     #hash document
-    hashed_shingles = [hash_f(hash_func, x) for x in shingles]
-    print "%s\t%s" % (min(hashed_shingles), video_id)
+    hashed_shingles = compute_hash_for_functions(hash_functions=hash_functions, shingles=shingles)
+    key = hashed_shingles
+    print "%s\t%s" % (key, video_id)
 
 
 if __name__ == "__main__":
     # Very important. Make sure that each machine is using the
     # same seed when generating random numbers for the hash functions.
     np.random.seed(seed=42)
-    hash_func = generate_hash_function()
-
+    hash_functions = generate_hash_functions(128)
     for line in sys.stdin:
         line = line.strip()
         video_id = int(line[6:15])
