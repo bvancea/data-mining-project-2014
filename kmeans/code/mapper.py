f176623be58e9235d8c4d9dfb0890fda4508de62
Bogdan Vancea <bogdan.aurel.vancea@gmail.com>
2014-05-13 10:18:39 +0200

    #3: Added online kmeans in the reducer.

--- kmeans/code/mapper.py
+++ kmeans/code/mapper.py
@@ -3,28 +3,15 @@ import sys
 import numpy as np
 
 SEPARATOR = ' '
-POINTS = 6667
 
 
 def main():
-    aggr_features = None
-    count = 0
-    indexes = np.random.random_integers(0, POINTS, 100)
-    indexes = sorted(indexes)
     for line in sys.stdin:
-        if count in indexes:
-            line = line.strip()
-            features = np.fromstring(line, sep=SEPARATOR)
-            if aggr_features is None:
-                aggr_features = features
-            else:
-                aggr_features = np.vstack((aggr_features, features))
-        count += 1
+        line = line.strip()
+        features = np.fromstring(line, sep=SEPARATOR)
+        value = features.tolist()
+        print "%s\t%s" % (1, value)
 
-    for point in aggr_features:
-        key = 1
-        value = point.tolist()
-        print "%s\t%s" % (key, value)
 
 if __name__ == "__main__":
     main()
--- kmeans/code/reducer.py
+++ kmeans/code/reducer.py
@@ -1,16 +1,61 @@
 #!/usr/bin/env python2.7
-import sys
 import numpy as np
-from sklearn import cluster
+from scipy.spatial.distance import cdist, euclidean
+import sys
 
 CLUSTERS = 200
-ITERATIONS = 40
+
+
+def online_kmeans(data, n_clusters, n_points):
+    #randomly assign clusters
+    center_indices = np.random.random_integers(0, n_points - 1, n_clusters)
+    centers = np.copy(data[center_indices])
+    cluster_counts = np.zeros(n_clusters)
+
+    #apply online update for all points
+    for point in data:
+        min_value = sys.maxint
+        min_index = 0
+        index = 0
+
+        #find nearest center
+        for center in centers:
+            dist = euclidean(center, point)
+            if dist < min_value:
+                min_index = index
+                min_value = dist
+            index += 1
+
+    #update count
+    cluster_counts[min_index] += 1
+    centers[min_index] += (1 / cluster_counts[min_index]) * (point - centers[min_index])
+    return centers
+
+
+def compute_cost(data, n_points, centers):
+    cost = 0
+    for point in data:
+        min_value = sys.maxint
+        index = 0
+
+        #find nearest center
+        for center in centers:
+            dist = euclidean(center, point)
+            if dist < min_value:
+                min_value = dist
+            index += 1
+        cost += (min_value * min_value)
+
+    cost = cost / n_points
+    return cost
 
 
 def main():
-    kmeans = cluster.KMeans(n_clusters=CLUSTERS, max_iter=ITERATIONS)
+    #kmeans = cluster.KMeans(n_clusters=CLUSTERS, max_iter=ITERATIONS)
     points = None
+    count = 0
     for line in sys.stdin:
+        count += 1
         line = line.strip()
         key, value = line.split("\t")
         point = np.array(eval(value))
@@ -19,9 +64,11 @@ def main():
         else:
             points = np.vstack((points, point))
 
-    kmeans.fit(points)
+    #kmeans.fit(points)
+    centers = online_kmeans(points, CLUSTERS, count)
+    #print "Cost: %d" % compute_cost(points, count, centers)
 
-    for center in kmeans.cluster_centers_:
+    for center in centers:
         print(' '.join(map(str, center)))
 
 if __name__ == "__main__":
